5508b569ca118a5a7a318b20ded4d2fb09ea7b81
Jdavid77 <jdavid72009@hotmail.com>
2021-11-05 15:41:23 +0000

    DonutChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -8,47 +9,47 @@ type ChartData = {
     series: number[];
 }
 
-const BarChart = () => {
+const DonutChart = () => {
 
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
+    useEffect(() => {
+        axios.get(BASE_URL + "/sales/amount-by-seller")
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const mylabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: mylabels, series: mySeries });
+                console.log(chartData);
+            });
+
+    }, []);
     // Forma Errada
-    let chartData : ChartData = { labels: [],series:[]};
-
-    axios.get(BASE_URL + "/sales/amount-by-seller")
-    .then( response => {
-        const data = response.data as SaleSum[];
-        const mylabels = data.map(x => x.sellerName);
-        const mySeries = data.map(x => x.sum);
-
-        chartData = {labels: mylabels , series: mySeries};
-        console.log(chartData);
-    });
-
-
 
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+
     const options = {
         legend: {
             show: true
         }
     }
-    
-    
+
+
     return (
-        <Chart 
-        
-            options={{...options, labels: chartData.labels}}
+        <Chart
+
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
-        
+
         />
     );
 }
 
-export default BarChart;
+export default DonutChart;
 
